8730610b5486492f31333f96cccc3ff23faa7765
Edson Borge <edsonborge90@gmail.com>
2020-09-28 17:53:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect , useState } from 'react';
+import Tmdb from './Tmdb';
+
+import Header from './components/Header';
+import FeaturedMovie from './components/FeaturedMovie';
+import MovieRow from './components/MovieRow';
+
 import './App.css';
 
-function App() {
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total dos filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o filme em destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <Header black={blackHeader} />
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow 
+            key={key}
+            title={item.title}
+            items={item.items}
+          />
+        ))}
+      </section>
+
+      <footer>
+        Feito por Edson Borge<br/> 
+        Direitos de imagem para <a href="http://netflix.com" target="_blank" rel="noopener noreferrer">Netflix</a><br/>
+        Dados fornecidos pelo site <a href="http://themoviedb.org" target="_blank" rel="noopener noreferrer">themoviedb.org</a>
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="loading"/>
+        </div>
+      }
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+export default ({ black }) => {
+  return (
+    <header className={black ? 'black' : ''}>
+      <div className="header--logo">
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix"/>
+        </a>
+      </div>
+
+      <div className="header--user">
+        <a href="/">
+          <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png" alt="User"/>
+        </a>
+      </div>
+    </header>
+  );
+}
